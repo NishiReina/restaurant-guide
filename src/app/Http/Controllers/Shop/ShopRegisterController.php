5ea0a112b562592a2c373c85c3183c083deb38ab
NishiReina <reina.nishi0609@gmail.com>
2023-10-29 17:01:18 +0900

    shop登録完了

--- src/app/Http/Controllers/Shop/ShopRegisterController.php
+++ src/app/Http/Controllers/Shop/ShopRegisterController.php
@@ -30,22 +30,31 @@ class ShopRegisterController extends Controller
     // 登録実行
     public function store(ShopRegisterRequest $request): RedirectResponse
     {
-        // $request->validate([
-        //     'name' => ['required', 'string', 'max:255'],
-        //     'email' => ['required', 'string', 'email', 'max:255', 'unique:'.Shop::class],
-        //     'password' => ['required', 'confirmed', Rules\Password::defaults()],
-        // ]);
-
         $img = $request->file('img_url');
 
         try {
             $img_url = Storage::disk('local')->put('public/img', $img);
-            $request->img_url = $img_url;
         } catch (\Throwable $th) {
             throw $th;
         }
 
-        $shop = Shop::create($request->all());
+        $shop = Shop::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => $request->password,
+            'phone' => $request->phone,
+            'postcode' => $request->postcode,
+            'address' => $request->address,
+            'start_time' => $request->start_time,
+            'end_time' => $request->end_time,
+            'min_price' => $request->min_price,
+            'max_price' => $request->max_price,
+            'area_id' => $request->area_id,
+            'gunre_id' => $request->gunre_id,
+            'img_url' => $img_url,
+            'title' => $request->title,
+            'pr' => $request->pr,
+        ]);
 
         event(new Registered($shop));
 
